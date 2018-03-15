b93cf54e2b1dc83d43a9bb7c80a004f3924f3d03
naonaorange <nao.lunalovegood@gmail.com>
2018-03-15 19:35:01 +0900

    create project using Prism

---
+++ SnippingTool/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SnippingTool.Views;
+
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using System.Windows;
+
+namespace SnippingTool
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            // this.ContainerでUnityのコンテナが取得できるので
+            // そこからShellを作成する
+            return this.Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            // Shellを表示する
+            ((Window)this.Shell).Show();
+        }
+    }
+}
